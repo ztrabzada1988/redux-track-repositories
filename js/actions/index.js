4683596566815db065667853cc292a3b0cfc1d39
Zubair Trabzada <zubair.trabzada@gmail.com>
2017-02-28 18:52:30 -0800

    updating reducers

---
+++ js/actions/index.js
@@ -0,0 +1,22 @@
+
+
+// to redue errors
+var ADD_REPOSITORY = 'ADD_REPOSITORY';
+export default function addRepository(repository) {
+    return {
+        type: ADD_REPOSITORY,
+        repository: repository
+    }
+};
+
+var RATE_REPOSITORY = 'RATE_REPOSITORY';
+export default function rateRepository(repository, rating) {
+    return {
+        type: RATE_REPOSITORY,
+        repository: repository,
+        rating: rating
+    };
+};
+
+exports.ADD_REPOSITORY = ADD_REPOSITORY;
+exports.RATE_REPOSITORY = RATE_REPOSITORY;
