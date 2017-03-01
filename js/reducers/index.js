e546301d5d397e09eeb55ff2734333e83923a25a
Zubair Trabzada <zubair.trabzada@gmail.com>
2017-02-28 18:10:28 -0800

    updating reducers

--- js/reducers/index.js
+++ js/reducers/index.js
@@ -14,8 +14,20 @@ export default function repositoryReducer(state, action) {
         const index = -1;
         for (let i = 0; i < state.length; i++) {
             const repository = state[i];
-
+            if (repository.name === action.repository) {
+                index = i;
+                break;
+            }
         }
+
+        if (index === -1) {
+            throw new Error('Could not find repository');
+        }
+
+        const before = state.slice(0, i);
+        const after = state.slice(i + 1);
+        const newRepository = Object.assign({}, repository, {rating: action.rating});
+        return before.concat(newRepository, after);
     }
     return state;
 };
