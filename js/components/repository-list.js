3a30cfbad22f149ed25e630690bc058aee3bbac0
Zubair Trabzada <zubair.trabzada@gmail.com>
2017-03-04 15:44:37 -0800

    creating and updating componenets

---
+++ js/components/repository-list.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Repository from './repository';
+
+export class RepositoryList extends React.Class {
+    addRepository: function() {
+        const repositoryName = this.refs.repositoryName.value;
+        // TODO: Add the repository to the state
+    },
+
+    render: function() {
+        const repositories = this.props.repositories.map(function(repository) {
+            return <Repository repository={repository} key={repository.name} />;
+        });
+
+        return (
+            <div className="repository-list">
+                {repositories}
+                <input type="text" ref="repositoryName" />
+                <button type="button" onClick={this.addRepository}>
+                    Add repository
+                </button>
+            </div>
+        );
+    }
+}
