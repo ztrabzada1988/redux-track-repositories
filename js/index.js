3709b431c308cd78afeaeb57830b51eaa46fa1c9
Zubair Trabzada <zubair.trabzada@gmail.com>
2017-03-04 16:30:26 -0800

    creating and updating components

--- js/index.js
+++ js/index.js
@@ -4,5 +4,10 @@ import ReactDOM from 'react-dom';
 import RepositoryList from './components/respository-list';
 
 document.addEventListener('DOMContentLoaded', function() {
-    ReactDOM.render(<RepositoryList />, document.getElementById('app'));
+    ReactDOM.render(
+        <Provider store={store}>
+            <RepositoryList />
+        </Provider>
+        document.getElementById('app')
+    );
 });
